797240b14f095a69e943b5180b6e609df273db21
DannielcoF <dannielco.solares@galileo.edu>
2021-08-10 08:19:02 -0600

    Add files via upload

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-export CLASSPATH=$CLASSPATH:/usr/class/cc4/cool/lib/java-cup-11a.jar:/usr/class/cc4/cool/lib/jlex.jar 
+export CLASSPATH=$CLASSPATH:/usr/class/cs143/cool/lib/java-cup-11a.jar:/usr/class/cs143/cool/lib/jlex.jar 
 java JLex.Main calculator.lex
 mv calculator.lex.java Yylex.java
 java java_cup.Main calculator.cup
